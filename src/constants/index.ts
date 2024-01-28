8157b8d205286073d0662f09baad108899a44c83
Muhammad Hassan <mh7722178@gmail.com>
2024-01-28 18:55:04 +0500

    portfolio

---
+++ src/constants/index.ts
@@ -0,0 +1,265 @@
+// Contains constant data for using in website
+// ! Don't remove anything from here if not sure
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  threejs,
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6,
+  user1,
+  user2,
+  user3,
+  youtube,
+  linkedin,
+  twitter,
+  github,
+} from "../assets";
+
+// Navbar Links
+export const NAV_LINKS = [
+  {
+    id: "about",
+    title: "About",
+    link: null,
+  },
+  {
+    id: "work",
+    title: "Work",
+    link: null,
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    link: null,
+  },
+
+] as const;
+
+// Services
+export const SERVICES = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Mobile Developer",
+    icon: mobile,
+  },
+] as const;
+
+// Technologies
+export const TECHNOLOGIES = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+] as const;
+
+// Experiences
+export const EXPERIENCES = [
+  {
+    title: "React.js Internship",
+    company_name: "internee.pk",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "September 2023 - October 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+] as const;
+
+// Testimonials
+export const TESTIMONIALS = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: user1,
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: user2,
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: user3,
+  },
+] as const;
+
+// Projects
+export const PROJECTS = [
+  {
+    name: "Disney+ Clone",
+    description:
+      "Disney+ is one of the biggest streaming platforms used by millions of people world-wide and allows us to stream high quality content in 4k and various other formats",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project1,
+    source_code_link: "https://github.com/MuhammadHassan03/disney-clone",
+    live_site_link: "https://mh-disney-clone.netlify.app/",
+  },
+  {
+    name: "Golds Gym",
+    description:
+      "Web application that enables users to search for fitness exercises, effective personalized positions, and recommends new exercises based on their personal preferences.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "rapidapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project2,
+    source_code_link: "https://github.com/MuhammadHassan03/fitness-app",
+    live_site_link: "https://mh-fitness-app.netlify.app/",
+  },
+  {
+    name: "Shoppy",
+    description:
+      "The most personalised admin dashboard web application that allows enables users to choose customized themes and dark mode with different pages and variety of charts.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "syncfusion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project3,
+    source_code_link: "https://github.com/MuhammadHassan03/admin-dashboard",
+    live_site_link: "https://github.com/MuhammadHassan03/admin-dashboard",
+  },
+  
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Linkedin",
+    icon: linkedin,
+    link: "https://www.linkedin.com/in/muhammadhassan03/",
+  },
+  {
+    name: "Twitter",
+    icon: twitter,
+    link: "https://twitter.com/mirza_hassan07",
+  },
+  {
+    name: "GitHub",
+    icon: github,
+    link: "https://github.com/MuhammadHassan03",
+  },
+] as const;
